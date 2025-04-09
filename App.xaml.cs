db7fc0a32335e8a417afd6be39a6af3a0a7ec8a9
omiso-note <pirukand@gmail.com>
2025-04-09 11:24:39 +0900

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ALCM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
