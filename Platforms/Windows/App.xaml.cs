2105adea811ba9a74f278ac65fe77f0c4ee5f804
omiso <pirukand@gmail.com>
2025-08-15 00:07:47 +0900

    ・デバッグ用 Windows Machine起動時の画面サイズ変更

--- Platforms/Windows/App.xaml.cs
+++ Platforms/Windows/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -17,6 +19,20 @@ namespace ALCM.WinUI
         public App()
         {
             this.InitializeComponent();
+
+            int WindowWidth = 393;
+            int WindowHeight = 851;
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+                var mauiWindow = handler.VirtualView;
+                var nativeWindow = handler.PlatformView;
+                nativeWindow.Activate();
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+
+            });
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
